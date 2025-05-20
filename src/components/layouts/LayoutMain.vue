264cb9f114ef9c09a5940e8424f92b1a42e4eb95
rafaelnath <rafaelnathanael41@yahoo.com>
2025-05-21 00:17:40 +0700

    added light dark theme switcher

--- src/components/layouts/LayoutMain.vue
+++ src/components/layouts/LayoutMain.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core';
 import Sidebar from '../Sidebar.vue';
+
+const isDarkTheme = useDark();
+const toggleDarkTheme = useToggle(isDarkTheme);
 </script>
 <template>
     <div class="w-full h-lvh flex gap-0.25">
@@ -8,7 +12,7 @@ import Sidebar from '../Sidebar.vue';
             <div class="w-full h-16 flex justify-between items-center bg-zinc-400 gap-6">
                 <div class="w-6 h-6 bg-zinc-200"></div>
                 <div class="p-1 rounded-md bg-primary text-on-primary text-xs">
-                    <p>DRK</p>
+                    <button @click="toggleDarkTheme()">{{ isDarkTheme ? "Light" : "Dark" }}</button>
                 </div>
             </div>
             <div class="w-full grow">
